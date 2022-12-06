23d15325c3838fa39f043aa94b68679f87351b00
yashpurkar <yashpurkar7079@gmail.com>
2022-12-06 20:15:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function reverseStr(str) {
+  // var listOfChars = str.split('');
+  // var reverseListOfChars = listOfChars.reverse();
+  // var reversedStr = reverseListOfChars.join(" ");
+  // return reversedStr;
+
+
+}
+
+document.write(reverseStr("Hii"));
